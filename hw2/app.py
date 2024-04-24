e9af9e0843f0cc13e14f06d5c9d6e02a44dd724d
Vipul Puttaswamy Gowda <vipulp@pdx.edu>
2024-04-23 19:34:37 -0700

    Added Terminal tool

---
+++ hw2/app.py
@@ -0,0 +1,27 @@
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.agents import load_tools
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+
+llm = GoogleGenerativeAI(model="gemini-pro")
+
+tools = load_tools(["llm-math","terminal"], llm=llm, allow_dangerous_tools=True)
+base_prompt = hub.pull("langchain-ai/react-agent-template")
+prompt = base_prompt.partial(instructions="Answer the user's request utilizing at most 8 tool calls")
+agent = create_react_agent(llm,tools,prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+print(f"Welcome to my application.  I am configured with these tools")
+for tool in tools:
+  print(f'  Tool: {tool.name} = {tool.description}')
+
+while True:
+    try:
+        line = input("llm>> ")
+        if line:
+            result = agent_executor.invoke({"input":line})
+            print(result)
+        else:
+            break
+    except Exception as e:
+        print(e)
